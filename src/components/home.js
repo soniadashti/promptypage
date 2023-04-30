e417a166eacf7ce9242d4a9954912e6320321b5c
soniadashti <sodashti@uw.edu>
2023-04-30 00:59:25 -0700

    more

--- src/components/home.js
+++ src/components/home.js
@@ -12,15 +12,22 @@ export default function Home() {
             <h1>Prompty</h1>
             </div>
             <div className="title-container">
-              <h2>Never Be at a Loss for Words with Chat Prompts to Connect with Your Distant Family</h2>
-              <p>A mobile application designed for you and your distant family members. With one-on-one chat prompts you can build
-             stronger relationships by having discussion topics made for you to get to know one another and start bridging the intergenerational gap.</p>
+              <h2>Break the Inter-generational Gap in Your Long Distance Family</h2>
+              <p>The only mobile application that gives you and your family chat prompts to get to know one</p>
             </div>
           </header>
           <div className="screens">
             <img src={`${process.env.PUBLIC_URL}/websiteimg/one.png`} alt="screen" className="screen-image"/>
             <img src={`${process.env.PUBLIC_URL}/websiteimg/two.png`} alt="screen" className="screen-image"/>
             <img src={`${process.env.PUBLIC_URL}/websiteimg/three.png`} alt="screen" className="screen-image"/>
+          </div>
+          <div className="overview">
+            <h2>Overview</h2>
+            <p className="mission">Problem Statement</p>
+            <p>Our mission is to bridge inter-generational gaps for distanced families. We found that talking to distanced
+              family members was challenging for many individuals as they did not know what to speak about. This led us to Prompty,
+              an app that gives you simple and easy prompted conversation topics for all ages. These prompts can give you more to talk
+              about with your family and get to know them past the surface level.</p>
           </div>
           <h2 className="text-center mt-4">Our Team</h2>
           <div className="our-team">

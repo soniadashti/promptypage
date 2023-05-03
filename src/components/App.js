8428b778a898405f22c892866674be75c29bd845
soniadashti <sodashti@uw.edu>
2023-05-03 00:24:30 -0700

    .

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ export default function App() {
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/prompts" element={<Prompts title="List of Prompts"/>} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />

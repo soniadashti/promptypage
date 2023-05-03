11fbbf0b2a28b1520f4a183fce1e33083d64480f
soniadashti <sodashti@uw.edu>
2023-05-03 00:02:53 -0700

    bugs

--- src/components/about.js
+++ src/components/about.js
@@ -7,27 +7,27 @@ export default function About() {
         <h1 className="text-center mt-4">Team Lingua</h1>
         <div className="our-team">
         <div className="image-container">
-        <img src="websiteimg/Neil.jpg" alt="Neil" />
+        <img src={`${process.env.PUBLIC_URL}/websiteimg/Neil.jpg`} alt="Neil" />
         <h4>Neil Dadlani</h4>
         <p>Project Manager</p>
         </div>
         <div className="image-container">
-        <img src="websiteimg/Sonia.jpg" alt="Sonia" />
+        <img src={`${process.env.PUBLIC_URL}/websiteimg/Sonia.jpg`} alt="Sonia" />
         <h4>Sonia Dashti</h4>
         <p>Product Manager & Designer</p>
         </div>
         <div className="image-container">
-        <img src="websiteimg/Travon.jpg" alt="Travon" />
+        <img src={`${process.env.PUBLIC_URL}/websiteimg/Travon.jpg`} alt="Travon" />
         <h4>Travon Dao</h4>
         <p>Full-Stack Developer</p>
         </div>
         <div className="image-container">
-        <img src="websiteimg/Saba.png" alt="Saba" />
+        <img src={`${process.env.PUBLIC_URL}/websiteimg/Saba.png`} alt="Saba" />
         <h4>Saba Tabatabai</h4>
         <p>UI/UX Designer</p>
         </div>
         <div className="image-container">
-        <img src="websiteimg/Jacob.jpg" alt="Jacob" />
+        <img src={`${process.env.PUBLIC_URL}/websiteimg/Jacob.jpg`} alt="Jacob" />
         <h4>Jacob Hitchcock</h4>
         <p>Front-End Developer</p>
         </div>
